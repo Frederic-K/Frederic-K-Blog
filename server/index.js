c394e87d9325037240c79ff168f82280a44ce9ff
Frederic-K <frederic.kreuder.pro@gmail.com>
2025-01-12 16:38:43 +0100

    fix helmet

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import helmet from 'helmet';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -26,6 +27,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
